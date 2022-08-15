a4d11357c468b66dc74f3f3be5fda3f8bfbfe971
john-coderpro <johnchrisdevelops@gmail.com>
2022-08-15 13:20:54 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+(function () {
+    const btnCta = document.querySelector('.submit');
+    btnCta.addEventListener('click', checkPassword);
+
+    function checkPassword() {
+        const errorMessage = document.querySelector('.error-message');
+        const passwordsNodeList = document.querySelectorAll('input[type=password]');
+        if (passwordsNodeList[0].value !== '' && passwordsNodeList[1].value !== '') {
+            if (passwordsNodeList[0].value !== passwordsNodeList[1].value) {
+
+                for (const elmt of passwordsNodeList) {
+                    errorMessage.innerHTML = 'Passwords do not match';
+                    elmt.setAttribute('class', 'error');
+                    elmt.addEventListener('input', () => {
+                        if (elmt.hasAttribute('class', 'error')) {
+                            elmt.removeAttribute('class', 'error');
+                            errorMessage.innerHTML = '&nbsp;';
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+})();
