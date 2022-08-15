4d2e50d2a9c8a401405e9ce8aaed6492e25eb3cb
john-coderpro <johnchrisdevelops@gmail.com>
2022-08-15 13:49:58 -0400

    Fix a bug caused by the form submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 (function () {
     const btnCta = document.querySelector('.submit');
-    btnCta.addEventListener('click', checkPassword);
+    btnCta.addEventListener('click', (e) => checkPassword(e));
 
-    function checkPassword() {
+    function checkPassword(e) {
         const errorMessage = document.querySelector('.error-message');
         const passwordsNodeList = document.querySelectorAll('input[type=password]');
         if (passwordsNodeList[0].value !== '' && passwordsNodeList[1].value !== '') {
             if (passwordsNodeList[0].value !== passwordsNodeList[1].value) {
-
+                e.preventDefault();
                 for (const elmt of passwordsNodeList) {
                     errorMessage.innerHTML = 'Passwords do not match';
                     elmt.setAttribute('class', 'error');
